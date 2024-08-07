dd270f287e4f27aadead9ed7933880dac8051dd0
eyoaab <eyobtariku48@gmail.com>
2024-08-07 17:51:56 +0300

    task 14: implmented and tesed local data using shared preferance

--- On-boarding/E-comerce/lib/data/dataSource/local_product_source.dart
+++ On-boarding/E-comerce/lib/data/dataSource/local_product_source.dart
@@ -1,11 +1,7 @@
 import 'dart:convert';
-
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_6/core/error/exception.dart';
-
 import '../model/product_model.dart';
-
-
 
 abstract class ProductLocalDataSource {
   Future<List<ProductsModel>> getCachedProducts();
@@ -13,22 +9,35 @@ abstract class ProductLocalDataSource {
 }
 
 class ProductLocalDataSourceImpl extends ProductLocalDataSource {
-  final SharedPreferences pref;
+  final SharedPreferences store;
 
-  ProductLocalDataSourceImpl({required this.pref});
+  ProductLocalDataSourceImpl({required this.store});
+
   @override
-  Future<bool> cacheProduct(ProductsModel productToCache) {
-    throw UnimplementedError();
+  Future<bool> cacheProduct(ProductsModel productToCache) async {
+    final List<String> jsonList = store.getStringList('cachedProducts') ?? [];
+    
+    final productJson = jsonEncode(productToCache.toJson());
+    jsonList.add(productJson);
+
+    final success = await store.setStringList('cachedProducts', jsonList);
+      print(success);
+
+    if (!success){
+      // print(success);
+      return false;
+    }
+    return true;
   }
 
   @override
-  Future<List<ProductsModel>> getCachedProducts() {
-    final jsonList = pref.getStringList('cachedProduct');
+  Future<List<ProductsModel>> getCachedProducts() async {
+    final jsonList = store.getStringList('cachedProducts');
 
     if (jsonList != null) {
-      return Future.value(jsonList
+      return jsonList
           .map((jsonString) => ProductsModel.fromJson(jsonDecode(jsonString)))
-          .toList());
+          .toList();
     }
 
     throw CacheException();
---
+++ On-boarding/E-comerce/test/data/dataSource/local_product_source_test.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_6/core/error/exception.dart';
+import 'package:task_6/data/dataSource/local_product_source.dart';
+import 'package:task_6/data/model/product_model.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  late MockSharedPreferences mockSharedPreferences;
+  late ProductLocalDataSourceImpl dataSource;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = ProductLocalDataSourceImpl(store: mockSharedPreferences);
+  });
+
+  group('cacheProduct', () {
+    const  product = ProductsModel(
+      id: 1,
+      name: 'Test Product',
+      description: 'Product Description',
+      imageUrl: 'http://example.com/image.jpg',
+      price: 99.99,
+    ); // Example product
+
+    test('should cache the product and return true', () async {
+      final jsonList = <String>[];
+      when(mockSharedPreferences.getStringList('cachedProducts')).thenReturn(jsonList);
+      when(mockSharedPreferences.setStringList('cachedProducts',[])).thenAnswer((_) async => true);
+
+      final result = await dataSource.cacheProduct(product);
+
+      final expectedJson = jsonEncode(product.toJson());
+      verify(mockSharedPreferences.setStringList('cachedProducts', [expectedJson])).called(1);
+      expect(result, true);
+    });
+  });
+
+  group('getCachedProducts', () {
+    const product = ProductsModel(
+      id: 1,
+      name: 'Test Product',
+      description: 'Product Description',
+      imageUrl: 'http://example.com/image.jpg',
+      price: 99.99,
+    ); 
+    final jsonList = [jsonEncode(product.toJson())];
+
+    test('should return list of products when cached data is available', () async {
+      
+      when(mockSharedPreferences.getStringList('cachedProducts')).thenReturn(jsonList);
+
+   
+      final result = await dataSource.getCachedProducts();
+
+     
+      expect(result, [product]);
+    });
+
+    test('should throw CacheException when no cached data is available', () async {
+     
+      when(mockSharedPreferences.getStringList('cachedProducts')).thenReturn(null);
+
+
+      final call = dataSource.getCachedProducts;
+
+    
+      expect(() => call(), throwsA(isA<CacheException>()));
+    });
+  });
+}
