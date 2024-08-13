38995a5b652b816be177ddf8200957357a12c0c4
eyoaab <eyobtariku48@gmail.com>
2024-08-14 00:57:26 +0300

    task 18 implmented the dependency injection

--- On-boarding/E-comerce/lib/main.dart
+++ On-boarding/E-comerce/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        
         BlocProvider<ProductBloc>(
           create: (context) => locator<ProductBloc>(), 
         ),
